a3f3b427670742f5028e635d6263a45669f43b01
suciendahssalimah-max <suciendahssalimah@gmail.com>
2025-12-14 20:24:10 +0700

    Add files via upload

---
+++ JS 1/profile_script.js
@@ -0,0 +1,10 @@
+let myName = "Suci Endah";
+const myAge = 16;
+
+console.log(Nama= " + myName");
+console.log(Umur= " + myAge");
+
+document.getElementById("nama-saya").innerHTML = "Halo, saya Suci Endah Salimah"
+document.getElementById("info-diri").innerHTML = "Saya berusia 16 tahun dan saya suka belajar pemrograman."
+
+alert("Anda telah mengklik tombol!");
